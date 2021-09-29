e48350e46efa044d18e7628553872944dfb81dbd
Ya Liu <sarahl0216@gmail.com>
2021-09-29 14:08:11 -0700

    trying out node module fast-csv

---
+++ data-loader.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+
+const csvData = [];
+fs.createReadStream(path.resolve(__dirname, 'assets', 'features.csv'))
+  .pipe(csv.parse({ maxRows: 10, headers: true }))
+  .on('error', (error) => console.error(error))
+  .on('data', (row) => {
+    // console.log(row);
+    csvData.push(row);
+  })
+  .on('end', () => console.log(csvData));
