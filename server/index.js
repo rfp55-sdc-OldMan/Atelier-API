715135c8e815e5a7a139a64513157e62c98915a9
Ya Liu <sarahl0216@gmail.com>
2021-09-30 13:52:33 -0700

    installed pg and express-promise-router, added server script

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log('connected to server at 3000');
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API' });
+});
+
+app.listen(port, () => {
+  console.log(`connected to server at ${port}`);
 });
