4b984e589c498a81ec0e358f61463000f0500b37
Ya Liu <sarahl0216@gmail.com>
2021-09-29 18:25:37 -0700

    more complete schema code

--- data-loader.js
+++ data-loader.js
@@ -3,7 +3,7 @@ const path = require('path');
 const csv = require('fast-csv');
 
 const csvData = [];
-fs.createReadStream(path.resolve(__dirname, 'assets', 'features.csv'))
+fs.createReadStream(path.resolve(__dirname, 'assets', 'styles.csv'))
   .pipe(csv.parse({ maxRows: 10, headers: true }))
   .on('error', (error) => console.error(error))
   .on('data', (row) => {
--- schema.sql
+++ schema.sql
@@ -1,17 +1,56 @@
+DROP TABLE product;
+
+CREATE TABLE product (
+  id SERIAL PRIMARY KEY,
+  name text NOT NULL,
+  slogan text,
+  description text,
+  category text,
+  default_price integer
+);
+
+DROP TABLE styles;
+
+CREATE TABLE styles (
+  id SERIAL PRIMARY KEY,
+  productId INTEGER REFERENCES product,
+  name text,
+  sale_price INTEGER DEFAULT NULL,
+  original_price INTEGER NOT NULL,
+  default_style boolean
+);
+
 DROP TABLE features;
 
 CREATE TABLE features (
-  id serial NOT NULL PRIMARY KEY,
-  product_id integer NOT NULL,
+  id SERIAL PRIMARY KEY,
+  product_id integer NOT NULL REFERENCES product,
   feature text,
-  value text,
+  value text
 );
 
 DROP TABLE photos;
 
 CREATE TABLE photos (
-  id serial NOT NULL,
+  id SERIAL PRIMARY KEY,
   styleId INTEGER REFERENCES styles,
   url text,
-  thumbnail_url text,
+  thumbnail_url text
 );
+
+DROP TABLE skus;
+
+CREATE TABLE skus (
+  id SERIAL PRIMARY KEY,
+  styleId INTEGER REFERENCES styles,
+  size text,
+  quantity integer
+);
+
+DROP TABLE related;
+
+CREATE TABLE related (
+  id SERIAL PRIMARY KEY,
+  current_product_id INTEGER REFERENCES product,
+  related_product_id INTEGER REFERENCES product
+)
