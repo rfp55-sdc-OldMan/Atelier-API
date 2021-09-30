947d75716a24951fa0ab2eacf939aca0dedb4b06
Ya Liu <sarahl0216@gmail.com>
2021-09-30 15:21:38 -0700

    added api endpoints based on fec documentation

--- server/index.js
+++ server/index.js
@@ -10,6 +10,22 @@ app.get('/', (req, res) => {
   res.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
+app.get('/products', (req, res) => {
+  res.json({ endpoint: 'list of products' });
+});
+
+app.get('/products/:product_id', (req, res) => {
+  res.json({ endpoint: '1 product' });
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  res.json({ endpoint: 'product styles' });
+});
+
+app.get('/products/:product_id/related', (req, res) => {
+  res.json({ endpoint: 'related products' });
+});
+
 app.listen(port, () => {
   console.log(`connected to server at ${port}`);
 });
