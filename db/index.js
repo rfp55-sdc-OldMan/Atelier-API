eb441984e981a884f35d6d06fddfd8c8421cd6d2
Ya Liu <sarahl0216@gmail.com>
2021-09-30 16:57:51 -0700

    successfully accessed postgresql table for 1 product at routes/product.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+  user: 'root',
+  host: 'localhost',
+  database: 'products-db',
+  password: '',
+  port: 5432,
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
