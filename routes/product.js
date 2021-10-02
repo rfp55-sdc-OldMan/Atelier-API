1e937039d6558cf9dcd8461e7bd11b7f930ca44e
Ya Liu <sarahl0216@gmail.com>
2021-10-02 11:50:02 -0700

    implemented subqueries for features

--- routes/product.js
+++ routes/product.js
@@ -9,7 +9,14 @@ module.exports = router;
 router.get('/:product_id', async (req, res) => {
   const id = req.params.product_id;
   const text = `SELECT
-   *
+   *,
+   json_build_object(
+     'features',
+     (SELECT json_agg(json_build_object(
+       'feature', features.feature,
+       'value', features.value
+     )) FROM features WHERE product_id = products.id)
+   )
   FROM products
   WHERE id = $1`;
 
