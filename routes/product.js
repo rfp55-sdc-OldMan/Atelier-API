3f7d9d8b68b97097adb7b0d7ac39d6ea48bb6697
Ya Liu <sarahl0216@gmail.com>
2021-10-02 11:59:55 -0700

    get 1 product query complete

--- routes/product.js
+++ routes/product.js
@@ -10,13 +10,11 @@ router.get('/:product_id', async (req, res) => {
   const id = req.params.product_id;
   const text = `SELECT
    *,
-   json_build_object(
-     'features',
-     (SELECT json_agg(json_build_object(
-       'feature', features.feature,
-       'value', features.value
-     )) FROM features WHERE product_id = products.id)
-   )
+    (SELECT json_agg(json_build_object(
+      'feature', feature,
+      'value', value
+    )) AS features FROM features WHERE product_id = products.id)
+
   FROM products
   WHERE id = $1`;
 
