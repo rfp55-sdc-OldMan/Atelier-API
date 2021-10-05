352249b11f49735639a67bf8a1130026d7e89639
Ya Liu <sarahl0216@gmail.com>
2021-10-04 20:37:17 -0700

    installed k6 globally, added tests for styles and product

--- tests/styles.js
+++ tests/styles.js
@@ -1,8 +1,15 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  vus: 3000,
+  duration: '15s',
+};
+
 export default function () {
   const id = Math.floor(Math.random() * 1000000);
   http.get(`http://localhost:3001/products/${id}/styles`);
   sleep(1);
 }
+
+// CLI command: k6 run test/script.js
